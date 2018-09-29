65c8aacaaadeefff695feac5d8eb5e257dde704d
Krutika Sawarkar <krutika.sawarkar@klingelnberg.com>
2018-09-29 19:00:17 +0530

    Fixed : Author changed

--- jsexercise1.js
+++ jsexercise1.js
@@ -2,7 +2,7 @@
 //make a function that organizes these into individual array that is ordered.
 //For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
 
-// @author - Mithilesh Tipkari
+// @author - Krutika Sawarkar
 
 const arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20]
 
